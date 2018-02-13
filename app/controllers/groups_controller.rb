9d98af759f4769084e6195a340688fb6fa863f74
TorreJohnson <torre.johnson@gmail.com>
2018-02-13 15:53:02 -0500

    added some css

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -7,6 +7,8 @@ class GroupsController < ApplicationController
 
   def create
     @group = Group.new(name: group_params[:name], creator_id: current_user.id, owner_id: current_user.id)
+    edit_group_members(@group)
+    @group.update(group_params)
     if @group.save
       redirect_to @group
     else
@@ -29,16 +31,14 @@ class GroupsController < ApplicationController
 
   def update
     @group = Group.find(params[:id])
-    if params[:groupaction] == 'join'
+    if params[:group_action] == 'join'
       @group.add_member(current_user)
       redirect_to groups_path
-    elsif params[:groupaction] == 'leave'
+    elsif params[:group_action] == 'leave'
       @group.remove_member(current_user)
       redirect_to groups_path
     else
-      members = params[:group][:member_ids].reject {|id| id.empty?}
-      members = members.map {|id| User.find(id)}
-      @group.members = members
+      edit_group_members(@group)
       @group.update(group_params)
       redirect_to @group
     end
@@ -62,6 +62,12 @@ class GroupsController < ApplicationController
       flash[:notice] = "You must be a member of this group to edit or delete"
       redirect_to @group
     end
+  end
+
+  def edit_group_members(group)
+    members = params[:group][:member_ids].reject {|id| id.empty?}
+    members = members.map {|id| User.find(id)}
+    group.members = members
   end
 
 end
