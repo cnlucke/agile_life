cf1f7378834d31c1f89bf75a6f73fff0e834aa8c
cnlucke <cristy.lucke@gmail.com>
2018-02-16 10:04:08 -0500

    added basic rspec tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,44 +7,47 @@ describe 'User' do
       password: "red",
   }}
 
-  it 'inherits from ActiveRecord::Base' do
-    expect(User.superclass).to eq(ActiveRecord::Base)
+  describe 'inherits from ActiveRecord::Base' do
+    expect(User.superclass.superclass).to eq(ActiveRecord::Base)
   end
 
   context 'User.new' do
-    let(:user) { User.new }
-    it 'has a name' do
-      user.name = "Mary"
-      expect(user.name).to eq("Mary")
+    before(:each) do
+      @user = User.new(name: "Mary", password: "red")
     end
 
-    it 'has a password' do
-      expect(user.password_digest_digest).to eq("$2a$10$yf7BL5878vHOG4IzcxFdRO0UEOfb0F3W/ZbP2xhQyt9CRgbJofkq.")
+    describe 'has a name' do
+      @user.name = "Mary"
+      expect(@user.name).to eq("Mary")
     end
 
-    it 'defaults to inactive' do
-      expect(user.active).to be_falsey
-    end
-
-    it 'can be updated to active' do
-      user.active = true
-      expect(user.active).to be_truthy
-    end
+    # describe 'has a password' do
+    #   expect(BCrypt::Password.create('red') == 'red').to be_truthy
+    # end
+    #
+    # describe 'defaults to inactive' do
+    #   expect(user.active).to be_falsey
+    # end
+    #
+    # describe 'can be updated to active' do
+    #   user.active = true
+    #   expect(user.active).to be_truthy
+    # end
 
   end
 
   context '::new' do
-    it 'cannot be instantiated without a name' do
+    describe 'cannot be instantiated without a name' do
       expect{User.new}.to raise_error
     end
 
-    it 'can be instantiated with a hash of attributes' do
+    describe 'can be instantiated with a hash of attributes' do
       expect{User.new(attributes)}.to_not raise_error
     end
   end
 
   context '#save' do
-    it 'can be saved to the database' do
+    describe 'can be saved to the database' do
       user = User.new(attributes)
       user.save
       expect(User.find_by(attributes)).to eq(user)
@@ -53,14 +56,14 @@ describe 'User' do
 
   context 'basic CRUD' do
     context 'creating' do
-      it 'can be instantiated and then saved' do
+      describe 'can be instantiated and then saved' do
         user = User.new
         user.name = "This is a name."
         user.save
         expect(User.find_by(name: "This is a name.").name).to eq("This is a title.")
       end
 
-      it 'can be created with a hash of attributes' do
+      describe 'can be created with a hash of attributes' do
         attributes = {
             name: "The Sting",
             password: 1973,
@@ -70,7 +73,7 @@ describe 'User' do
         expect(User.find_by(attributes)).to eq(user)
       end
 
-      it 'can be created in a block' do
+      describe 'can be created in a block' do
 
         user =  User.create do |u|
                   u.name = "Home Alone"
@@ -90,37 +93,37 @@ describe 'User' do
         end
       end
 
-      it 'can get the first item in the database' do
+      describe 'can get the first item in the database' do
         user = User.first.title
         expect(user).to eq("User_0")
       end
 
-      it 'can get the last item in the databse' do
+      describe 'can get the last item in the databse' do
         user = User.last.title
         expect(user).to eq("User_4")
       end
 
-      it 'can get size of the database' do
+      describe 'can get size of the database' do
         users_size = User.count
         expect(users_size).to eq(5)
       end
 
-      it 'can retrive the first item from the database by id' do
+      describe 'can retrive the first item from the database by id' do
         expect(User.find_by(id: 1).name).to eq("User_0")
       end
 
-      it 'can retrieve from the database using different attributes' do
+      describe 'can retrieve from the database using different attributes' do
         user = User.create(name: "Name", password: "red")
         expect(User.find_by(name: "Title", password_digest: BCrypt::Password.create("2000"))).to eq(user)
       end
 
-      it 'can use a where clause and be sorted' do
+      describe 'can use a where clause and be sorted' do
         expect(User.where("name > ?", "a").order(name: :desc).map{|u| u.name}).to eq(["User_4", "User_3"])
       end
     end
 
     context 'updating' do
-      it 'can be found, updated, and saved' do
+      describe 'can be found, updated, and saved' do
         user = User.create(name: "Awesome Flick", password: "red")
         expect {
           user = User.find_by(name: "Awesome Flick")
@@ -129,12 +132,12 @@ describe 'User' do
         }.to change{ user.name }.from("Awesome Flick").to("Even Awesomer Flick")
       end
 
-      it 'can be updated using #update' do
+      describe 'can be updated using #update' do
         User.create(name: "Wat?").update(:name => "Wat, huh?")
         expect(User.find_by(name: "Wat, huh?")).to_not be_nil
       end
 
-      it 'can update all records at once' do
+      describe 'can update all records at once' do
         5.times do |i|
           User.create(name: "User_#{i}", password_digest: 2000+i)
         end
@@ -144,12 +147,12 @@ describe 'User' do
     end
 
     context 'destroying' do
-      it 'can destroy a single item' do
+      describe 'can destroy a single item' do
         User.create(name: "That One Where the Guy Kicks Another Guy Once").destroy
         expect(User.find_by(name: "That One Where the Guy Kicks Another Guy Once")).to be_nil
       end
 
-      it 'can destroy all items at once' do
+      describe 'can destroy all items at once' do
         10.times do |i|
           User.create(name: "User_#{i}")
         end
